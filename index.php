da8d6629aac17097635a4257d1052fb3f8f28392
VladimKoch <vkochan@tiscali.cz>
2025-01-30 10:58:36 +0100

    New

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="card">
+    <div class="cover">
+        <img src="kniha.jpg" alt="" >
+        <img src="black-gold.jpg" alt="" style="width: 94%;height:95%;margin-top:10px;padding-right:0.5px">
+    </div>
+    <div class="content">
+        <h1>Bible</h1>
+        <p>Český studíjní překlad</p>
+        <br>
+        <a href="#">více</a>
+    </div>
+</div>
+
+    
+</body>
+</html>
+<script src="js.js"></script>
